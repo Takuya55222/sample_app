e8678957d61ff14ecc92e5b78fdfab3593b0b9d7
Takuya <takuya1980222@gmail.com>
2020-07-12 15:06:03 +0900

    finish ch11 re

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
@@ -11,11 +13,9 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-<<<<<<< HEAD
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
-=======
-end
->>>>>>> b01c892cfc5675b9f2dc2488a8a72c709fd43898
